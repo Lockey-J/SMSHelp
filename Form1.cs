ed627bbedf05940ba9aca532f6c38aaf5cd5fd1e
Lockey-J <zhijia21cn@163.com>
2020-02-25 08:25:10 +0800

    更新爱接码平台所有接口

--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using SMSBase.Base;
 namespace SMSHelp
 {
     public partial class Form1 : Form
@@ -15,6 +15,15 @@ namespace SMSHelp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {                       
+            var clsaima = new AiMaSMSBase();
+            Console.WriteLine(clsaima.Login("lin01ok","6565810ok"));
+            Console.WriteLine(clsaima.Token);
+            Console.WriteLine(clsaima.GetPhoneMsg("21472","13556489648",out string aa));
+            Console.WriteLine(clsaima.ErrMsg);
         }
     }
 }
